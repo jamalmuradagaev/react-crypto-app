18d07b65d63aa1397829d5a97bad00dd5e93432f
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-04-30 20:32:56 +0300

    подключена библиотека antd и при помощи нее написана базовая разметка приложения

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Layout } from 'antd';
 
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 60,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 60px)',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#001529',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Layout>
+        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout>
+          <Layout.Sider width="25%" style={siderStyle}>
+            Sider
+          </Layout.Sider>
+          <Layout.Content style={contentStyle}>Content</Layout.Content>
+        </Layout>
+      </Layout>
+    </div>
   )
 }
 
-export default App
+
