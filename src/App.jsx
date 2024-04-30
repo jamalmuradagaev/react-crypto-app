754edf47ebf6a46093c82377b06f0ddf73e3448b
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-04-30 21:06:56 +0300

    были созданы файлы data.js для хранения данных и api.js для запроса к этим данным

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
 import React from 'react';
 
 import { Layout } from 'antd';
-
-const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-const contentStyle = {
-  textAlign: 'center',
-  minHeight: 'calc(100vh - 60px)',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#001529',
-};
-const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
+import AppHeader from './components/layout/AppHeader';
+import AppSider from './components/layout/AppSider';
+import AppContent from './components/layout/AppContent';
 
 export default function App() {
   return (
-    <div className='App'>
+    <Layout>
+      <AppHeader />
       <Layout>
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
-        <Layout>
-          <Layout.Sider width="25%" style={siderStyle}>
-            Sider
-          </Layout.Sider>
-          <Layout.Content style={contentStyle}>Content</Layout.Content>
-        </Layout>
+        <AppSider />
+        <AppContent />
       </Layout>
-    </div>
+    </Layout>
   )
 }
 
